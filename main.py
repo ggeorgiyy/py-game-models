89ecf15e9aef9a158827d384fd4d2f913a8e7935
Heorhii Zashchuk <ggeorgiyy@gmail.com>
2025-04-15 15:15:09 +0300

    Solution

--- main.py
+++ main.py
@@ -9,3 +9,30 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+    obj, created = Race.objects.get_or_create(
+        name="Humans",
+        description ="Very nice and smart creatures"
+    )
+
+    obj, created = Skill.objects.get_or_create(
+        name="Tutor",
+        bonus ="Can train other creatures"
+    )
+
+    obj, created = Guild.objects.get_or_create(
+        name="Rebellion_1",
+        description ="Fight against the Empire"
+    )
+
+    Players = {}
+    try:
+        obj = Player.objects.get(nickname="10", email="10@gmai.com")
+        for key, value in Players.items():
+            setattr(obj, key, value)
+        obj.save()
+    except Player.DoesNotExist:
+        new_values = {"nickname": "10", "email": "10@gmai.com"}
+        new_values.update(Players)
+        obj = Player(**new_values)
+        obj.save()
+
