3099fb7debefdc86a4bac17cae5a3ee4ffb36ad1
Heorhii Zashchuk <ggeorgiyy@gmail.com>
2025-05-07 20:28:20 +0300

    Solution: flake8 has been implemented

--- db/models.py
+++ db/models.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from django.db import models
 
 
@@ -6,20 +5,29 @@ class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
+
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skills')
+    race = models.ForeignKey(Race,
+                             on_delete=models.CASCADE,
+                             related_name='skills'
+                             )
+
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
+
 class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='players')
-    guild = models.ForeignKey(Guild, on_delete=models.CASCADE, related_name='players')
+    race = models.ForeignKey(Race,
+                             on_delete=models.CASCADE,
+                             related_name='players')
+    guild = models.ForeignKey(Guild,
+                              on_delete=models.CASCADE,
+                              related_name='players')
     created_at = models.DateTimeField(auto_now_add=True)
-
--- main.py
+++ main.py
@@ -36,7 +36,5 @@ def main() -> None:
     )
 
 
-
-
 if __name__ == "__main__":
     main()
